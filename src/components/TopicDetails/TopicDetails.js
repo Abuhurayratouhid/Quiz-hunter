a7ac2d777358bc3a027a0117d7e88e92b0e043e0
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-10-11 20:36:58 -0700

    quiz result showing using toast

--- src/components/TopicDetails/TopicDetails.js
+++ src/components/TopicDetails/TopicDetails.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Quiz from '../Quiz/Quiz';
 
 const TopicDetails = () => {
@@ -15,9 +17,17 @@ const TopicDetails = () => {
         const clickedOption= option;
         const rightAnswer = correctAnswer;
         if(clickedOption === rightAnswer){
+           toast.success ('Correct answer', {
+            position: "top-center",
+            autoClose: 500,
+           })
             console.log('true')
         }
         else{
+            toast.warn('Wrong answer', {
+                position: "top-center",
+                autoClose: 500,
+               })
             console.log('false');
         }
         // console.log(clickedOption,  rightAnswer)
@@ -31,7 +41,7 @@ const TopicDetails = () => {
                 handleCorrectAnswer={handleCorrectAnswer} 
                 ></Quiz>)
             }
-            
+          <ToastContainer></ToastContainer>  
         </div>
     );
 };
