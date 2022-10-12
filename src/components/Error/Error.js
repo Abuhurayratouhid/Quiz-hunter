dbf1036c90fc2b6bb316a7dee8657791f2c4f4be
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-10-11 22:04:55 -0700

    error page added

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div>
-            <p>Ops .....  Page not found </p>
+        <div className='text-red-700  bg-red-100 text-3xl mt-80 w-40 h-40 ml-[45%]  rounded-full'>
+            <p>Ops ..... <br /> Page not found </p>
             <p>404 </p>
         </div>
     );
